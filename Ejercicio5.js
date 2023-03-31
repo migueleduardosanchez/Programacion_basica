def14ad355d5df7dfec5d157115bda2f304f2eef
Miguel Sánchez <a324795@uach.mx>
2023-03-31 13:33:35 -0600

    5.-Hacer un programa que imprima los números impares hasta el 100 y que imprima cuantos impares hay.

---
+++ Ejercicio5.js
@@ -0,0 +1,12 @@
+// 5.-Hacer un programa que imprima los números impares hasta el 100 y que
+// imprima cuantos impares hay.
+
+var j = 1;
+console.log(j);
+
+for (var i = 1; i < 50; i++){
+  j = 2*i+1;
+  console.log(j);
+}
+
+console.log('Hay ' + i + ' impares');
