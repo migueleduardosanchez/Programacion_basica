9f2112413210418127048e466ff746d67f1bb461
Miguel Sánchez <a324795@uach.mx>
2023-04-03 22:01:08 -0600

    28.- Simular el lanzamiento de una moneda al aire e imprimir cara o cruz.

---
+++ Ejercicio28.js
@@ -0,0 +1,9 @@
+// 28.- Simular el lanzamiento de una moneda al aire e imprimir cara o cruz.
+
+let moneda = Math.floor(Math.random() * 2);
+
+if (moneda == 1) {
+    console.log("Cara");
+} else {
+    console.log("Cruz");
+}
