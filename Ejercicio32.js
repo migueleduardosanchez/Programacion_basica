7802ee59597d285acc4b85746074e3e0b8d7ab00
Miguel Sánchez <a324795@uach.mx>
2023-04-03 23:11:58 -0600

    32.- Crear un arreglo de 20 elementos con nombres de personas. Mostrar los elementos de la lista debiendo ir cada uno en una fila distinta.

---
+++ Ejercicio32.js
@@ -0,0 +1,11 @@
+// 32.- Crear un arreglo de 20 elementos con nombres de personas. Mostrar los
+// elementos de la lista debiendo ir cada uno en una fila distinta.
+
+let nombres = ["Miguel", "Eduardo", "Alexis", "Jack", "Jesus", "Mario",
+              "Andres", "Hector", "Rene", "Ivan", "Tarcicio", "Angel", "Luis",
+              "Fernando", "Memo", "Salem", "Alberto", "Humberto", "Gloria",
+              "Mayda"];
+
+for (var i = 0; i < nombres.length; i++) {
+    console.log(nombres[i]);
+}
